8186b27501788a81b2e786b5a322212a06880336
stepkj <karthikrpj2002@gmail.com>
2021-09-30 20:21:01 +0530

    Searchfilter page

--- lib/Searchfilter.dart
+++ lib/Searchfilter.dart
@@ -3,27 +3,29 @@ import 'package:api_project/constants.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:api_project/model.dart';
-
-Future<all> getData(String query) async {
-  var alll;
-  var response = await http.get(
-    Uri.parse('https://api.github.com/search/repositories?q=$query'),
-    headers: {"Accept": "application/json"},
-  );
-
-  if (response.statusCode == 200) {
-    var jsonString = response.body;
-    Map<String, dynamic> jsonMap = json.decode(jsonString);
-
-    alll = all.fromJson(jsonMap);
-    return alll;
-  } else {
-    throw Exception('Failed to fetch repos!');
-  }
-}
+import 'package:api_project/searchpage.dart';
 
 class Search extends StatefulWidget {
-  const Search({Key? key}) : super(key: key);
+  final String search;
+  Search(this.search);
+
+  Future<all> getData(String query) async {
+    var alll;
+    var response = await http.get(
+      Uri.parse('https://api.github.com/search/repositories?q=$query'),
+      headers: {"Accept": "application/json"},
+    );
+
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+      alll = all.fromJson(jsonMap);
+      return alll;
+    } else {
+      throw Exception('Failed to fetch repos!');
+    }
+  }
 
   @override
   _SearchState createState() => _SearchState();
